4abd296d4dcb9b1c52e7d4f09e42c9c2ef834761
olehhapuk <oleggapuk20@gmail.com>
2021-04-21 19:26:23 +0300

    Finished constructors

--- module-05/constructor.js
+++ module-05/constructor.js
@@ -64,27 +64,108 @@
 // console.log(`hotel1`, hotel1);
 // console.log(`hotel2`, hotel2);
 
-function Manager(name, initialSales) {
-  this.name = name;
-  this.sales = initialSales;
-}
+// function Manager(name, initialSales) {
+//   this.name = name;
+//   this.sales = initialSales;
+// }
 
-Manager.prototype.sell = function () {
-  this.sales += 1;
+// Manager.prototype.sell = function () {
+//   this.sales += 1;
+// };
+
+// const john = new Manager('John', 3);
+
+// john.sell();
+// john.sell();
+// john.sell();
+
+// console.log(john);
+
+// const din = new Manager('Din', 10);
+
+// din.sell();
+// din.sell();
+// din.sell();
+
+// console.log(din);
+
+// Task - 1
+
+/*
+const obj = {
+  age: 25,
+  func() {
+    this.age;
+  },
 };
 
-const john = new Manager('John', 3);
+obj.func();
 
-john.sell();
-john.sell();
-john.sell();
+function User(firstName, lastName, initialAge) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = initialAge;
+}
+
+User.prototype.getInfo = function () {
+  console.log(`${this.firstName} ${this.lastName} is ${this.age} years old`);
+};
+
+const john = new User('John', 'Doe', 25);
+const micheal = new User('Micheal', 'Ramires', 23);
+
+john.age = 26;
+
+john.getInfo(); // John Doe is 25 years old
+micheal.getInfo(); // John Doe is 25 years old
 
 console.log(john);
+*/
 
-const din = new Manager('Din', 10);
+/*
+// Constructor example
 
-din.sell();
-din.sell();
-din.sell();
+function ConstructorFunc(initialItems) {
+  const obj = {};
 
-console.log(din);
+  obj.items = initialItems;
+
+  return obj;
+}
+
+const testStorage = ConstructorFunc(['Item 1', 'Item 2']);
+*/
+
+function Storage(initialItems) {
+  Storage.count++;
+  this.items = initialItems;
+}
+
+Storage.prototype.getItems = function () {
+  console.log(this.items);
+};
+
+Storage.prototype.addItem = function (item) {
+  this.items.push(item);
+};
+
+Storage.prototype.removeItem = function (item) {
+  this.items.splice(this.items.indexOf(item), 1);
+};
+
+Storage.count = 0;
+
+// ---------------------
+
+const storageObject = new Storage(['flowers', 'candies']);
+const storageObject1 = new Storage(['candies']);
+
+storageObject.addItem('apples');
+storageObject.getItems();
+
+storageObject1.removeItem('candies');
+storageObject1.getItems();
+
+// Static methods
+
+console.log(Storage.count);
