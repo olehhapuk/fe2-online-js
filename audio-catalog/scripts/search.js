b5bed86335b68758009c2906af0927fe8c5bf82d
olehhapuk <oleggapuk20@gmail.com>
2021-05-24 19:29:59 +0300

    Search page done

--- audio-catalog/scripts/search.js
+++ audio-catalog/scripts/search.js
@@ -1,1 +1,23 @@
-console.log('Search page loaded');
+const typesListElem = document.querySelector('.types-list');
+const typeElements = document.querySelectorAll('.type');
+
+// const types = ['Album', 'Artist'];
+
+// for (const type of types) {
+//   const span = document.createElement('span');
+//   span.textContent = type;
+// }
+
+typesListElem.addEventListener('click', (e) => {
+  const targetElem = e.target.closest('.type');
+
+  if (targetElem === null) {
+    return;
+  }
+
+  for (const typeElem of typeElements) {
+    typeElem.classList.remove('type_active');
+  }
+
+  targetElem.classList.add('type_active');
+});
